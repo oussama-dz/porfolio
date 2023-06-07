e700a955510d3f37c141eb33ada716062458c183
CHERFAOUI Oussama <64999736+oussama-dz@users.noreply.github.com>
2023-06-07 09:46:49 +0100

    update 1

--- app.js
+++ app.js
@@ -52,7 +52,8 @@ const cardsData = [
     canopy: 'Food Order',
     koko: '',
     desc: 'A Food Order app for a restaurant.',
-    breif: 'A Food Order app for a restaurant. ',
+    breif:
+      'This food ordering app makes it easy for users to order their favorite meals from a restaurant. With a wide selection of food items to choose from, a search function, and the ability to save favorite items, users can order food quickly and easily. The app also allows users to add multiple locations and choose a location when they place an order, making it even more convenient. With both a dark and light theme and a deals section, this app provides a user-friendly and enjoyable ordering experience. ',
   },
   {
     className: ['tonic', 'order1'],
@@ -61,10 +62,11 @@ const cardsData = [
     skills: ['Kotlin', 'Jetpack Compose', 'Android'],
     modalSkills: new Set(['Kotlin', 'Jetpack Compose']),
     title: 'Calorie Counter',
-    canopy: 'Calorie Tracker',
+    canopy: 'Calorie And Food Tracker',
     koko: '',
     desc: 'A calorie tracker android app.',
-    breif: 'A calorie tracker android app. ',
+    breif:
+      'This calorie tracker app is designed to help users achieve their health and fitness goals by tracking their daily caloric intake. With user inputs such as age, weight, height, and gender, the app calculates the required daily caloric intake. Users can log their daily meals and group them by meal type, search the food database, and make informed decisions about what to eat. The app features both a dark and light theme and a user-friendly interface, making it easy for users to track their progress towards their goals. ',
   },
   {
     className: ['tonic', 'order2'],
@@ -76,7 +78,8 @@ const cardsData = [
     canopy: 'Plant Watering Reminder',
     koko: '',
     desc: 'A plant watering reminder App.',
-    breif: 'A Plant watering reminder app. ',
+    breif:
+      "This plant watering reminder app helps users keep their plants healthy and thriving by providing reminders to water them. Users can add their plants with pictures, set up reminders, and receive notifications when it's time to water each plant. The app also allows users to add information about each plant, such as its name and placement, and features a search and filter functionality for easy organization. With a user-friendly interface and visually appealing design, this app is a must-have for anyone who loves plants. ",
   },
 ];
 
